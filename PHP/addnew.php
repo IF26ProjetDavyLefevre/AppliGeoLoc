08fcc4087dbc5a26b8e9d0987ba385103d936d79
Pierre Davy <pierredavy92@gmail.com>
2014-12-02 16:46:55 +0100

    Get en cours

--- PHP/addnew.php
+++ PHP/addnew.php
@@ -2,18 +2,45 @@
 
 require 'BDD.php';
 
-
+$parameters = array
+    (
+    'login' => null,
+    'password' => null,
+    'token'=>null,
+    'update'=>null,
+    'coordonnees'=>null
+);
 
 $db = new BDD();
 $db->DB();
 
+foreach ($_GET as $key => $value) {
+    $parameters["$key"] = $value;
+}
+
+
+//http://pierredavy.com/addnew.php?login=pierredavy&password=123&token=987654321&update=2014-12-02&coordonnees=24:12
+
+$login = $parameters['login'];
+$mdp = $parameters['password'];
+$token = $parameters['token'];
+$update = $parameters['update'];
+$coordonnees = $parameters['coordonnees'];
+print_r($parameters);
+echo "\r".$login." ".$mdp." ".$token." ".$update." ".$coordonnees;
+
+
+
+//latitude , longitude, altitude, précision
+
+
+
+/*
 $login = "login";
-$mdp="mdp";
-$token="1";
-$update="2014/12/02";
-$coordonnees="12:23";
+$mdp = "mdp";
+$token = "1";
+$update = "2014/12/02";
+$coordonnees = "12:23";*/
 
-$db->AddNewUser($login,$mdp,$token,$update,$coordonnees);
-
-
+$db->AddNewUser($login, $mdp, $token, $update, $coordonnees);
 ?>
