18a2f6ca9eaf1c03c9da37ff6a1c0d53a93d7411
Pierre Davy <pierredavy92@gmail.com>
2014-12-08 21:00:21 +0100

    OMG LES ONGLETS MARCHENT !

--- IF26DavyLefevre/app/src/main/java/com/example/pierre/if26davylefevre/MainPage.java
+++ IF26DavyLefevre/app/src/main/java/com/example/pierre/if26davylefevre/MainPage.java
@@ -1,39 +1,67 @@
 package com.example.pierre.if26davylefevre;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 
 
 public class MainPage extends Activity {
 
-    @Override
+
+    ActionBar.Tab tab1, tab2, tab3;
+    Fragment fragmentTab1 = new Tab1Fragment();
+    Fragment fragmentTab2 = new Tab2Fragment();
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        tab1 = actionBar.newTab().setText("1");
+        tab2 = actionBar.newTab().setText("2");
+
+
+        tab1.setTabListener(new MyTabListener(fragmentTab1));
+        tab2.setTabListener(new MyTabListener(fragmentTab2));
+
+
+        actionBar.addTab(tab1);
+        actionBar.addTab(tab2);
+
     }
 
+    public class MyTabListener implements ActionBar.TabListener {
+        Fragment fragment;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_page, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public MyTabListener(Fragment fragment) {
+            this.fragment = fragment;
         }
 
-        return super.onOptionsItemSelected(item);
+        public void onTabSelected(Tab tab, FragmentTransaction ft) {
+            ft.replace(R.id.fragment_container, fragment);
+        }
+
+        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+            ft.remove(fragment);
+        }
+
+        public void onTabReselected(Tab tab, FragmentTransaction ft) {
+            // nothing done here
+        }
     }
 }
---
+++ IF26DavyLefevre/app/src/main/java/com/example/pierre/if26davylefevre/Tab2Fragment.java
@@ -0,0 +1,22 @@
+package com.example.pierre.if26davylefevre;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Tab2Fragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_tab2, container, false);
+        TextView textview = (TextView) view.findViewById(R.id.tabtextview2);
+        textview.setText(R.string.body2);
+        return view;
+    }
+}
