e3d07f736e5a038a212e06af03b94742a076e50b
Pierre Davy <pierredavy92@gmail.com>
2014-12-09 13:19:57 +0100

    refactoring

--- IF26DavyLefevre/app/src/main/java/com/example/pierre/if26davylefevre/Tab2Fragment.java
+++ IF26DavyLefevre/app/src/main/java/com/example/pierre/if26davylefevre/Tab2Fragment.java
@@ -1,6 +1,8 @@
 package com.example.pierre.if26davylefevre;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -9,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -19,18 +23,41 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.MapView;
+import android.graphics.drawable.Drawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
-public class Tab2Fragment extends Fragment {
 
-    MapView m;
+public class Tab2Fragment extends Fragment implements LocationListener{
+
+    MapView map;
+    private LocationManager locationManager;
+    private String provider;
+    int lat;
+    int lng;
+    //LocationOverlay  myLocOverlay;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // inflat and return the layout
         View v = inflater.inflate(R.layout.fragment_tab2, container, false);
-        m = (MapView) v.findViewById(R.id.mapView);
-        m.onCreate(savedInstanceState);
+        map = (MapView) v.findViewById(R.id.mapView);
+        map.onCreate(savedInstanceState);
 
         return v;
     }
@@ -39,24 +66,44 @@ public class Tab2Fragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        m.onResume();
+        map.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        m.onPause();
+        map.onPause();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        m.onDestroy();
+        map.onDestroy();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        m.onLowMemory();
+        map.onLowMemory();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
     }
 }
