2db300080c27dfd9db8ccbbd1d30032b1180c4a9
Pierre Davy <pierredavy92@gmail.com>
2014-12-09 11:12:09 +0100

    Google MAPS WORK B**tch

--- IF26DavyLefevre/app/src/main/java/com/example/pierre/if26davylefevre/Tab2Fragment.java
+++ IF26DavyLefevre/app/src/main/java/com/example/pierre/if26davylefevre/Tab2Fragment.java
@@ -9,15 +9,53 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.MapView;
 
 public class Tab2Fragment extends Fragment {
 
+    MapView m;
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_tab2, container, false);
-        TextView textview = (TextView) view.findViewById(R.id.tabtextview2);
-        textview.setText(R.string.body2);
-        return view;
+                             Bundle savedInstanceState) {
+        // inflat and return the layout
+        View v = inflater.inflate(R.layout.fragment_tab2, container, false);
+        m = (MapView) v.findViewById(R.id.mapView);
+        m.onCreate(savedInstanceState);
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        m.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        m.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        m.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        m.onLowMemory();
     }
 }
-
