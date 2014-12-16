92f7c0ad680e4094723708d099592ddac60cae40
Lefèvre Valentin <lefevre.vqrm@gmail.com>
2014-12-16 14:13:53 +0100

    Update récupération données contacts
    pas encore affichés sur la map

--- IF26DavyLefevre/app/src/main/java/com/example/pierre/if26davylefevre/Map_Activity.java
+++ IF26DavyLefevre/app/src/main/java/com/example/pierre/if26davylefevre/Map_Activity.java
@@ -36,7 +36,9 @@ public class Map_Activity extends Activity implements LocationListener {
     private double altitude;
     private float accuracy;
     private Marker myPosition;
+    private Marker[] contactMarker;
     private LatLng me;
+    private LatLng[] contactLatLng;
     private String login;
     private Location location;
     private String token;
@@ -79,6 +81,22 @@ public class Map_Activity extends Activity implements LocationListener {
         // on thread la récupération des informations de l'utilisateur
         ThreadMapActivity DOC = new ThreadMapActivity();
         DOC.execute(login, token);
+        try {
+            synchronized (this){
+                wait(3000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        contactLatLng = new LatLng[tabUser.length];
+        contactMarker = new Marker[tabUser.length];
+        Double latitudeContact;
+        Double longitudeContact;
+        for(int i = 0; i <tabUser.length; i++ ) {
+            //latitudeContact = Double.parseDouble(tabUser[i][1]);
+            //longitudeContact = Double.parseDouble(tabUser[i][2]);
+            //contactLatLng[i] = new LatLng(latitudeContact, longitudeContact);
+        }
         String msg = "Latitude ="+latitude+", Longitude = "+longitude;
         //Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
         me = new LatLng(latitude,longitude);
@@ -142,10 +160,18 @@ public class Map_Activity extends Activity implements LocationListener {
             // et on récupère les valeurs voulues, pour l'instant ça ne marche pas vu que la page php n'existe pas
             try {
                 JSONObject userList = new JSONObject(result);
-                JSONArray userArray = userList.getJSONArray("contacts");
+                JSONArray userArray = userList.getJSONArray("user");
                 tabUser = new String[userArray.length()][3];
-                latitude = Double.parseDouble(msg.getString("latitude").toString());
-                longitude = Double.parseDouble(msg.getString("longitude").toString());
+                Log.d("Nombre de contact", " "+userArray.length());
+                for (int i = 0; i < userArray.length()-1; i ++){
+                    tabUser[i][0] = userArray.getJSONObject(i+1).getString("login").toString();
+                    tabUser[i][1] = userArray.getJSONObject(i + 1).getString("latitude");
+                    tabUser[i][2] = userArray.getJSONObject(i + 1).getString("longitude");
+                    Log.d("Contenu tabUser", "login : "+tabUser[i][0]+", latitude : "+tabUser[i][1]+", longitude : "+tabUser[i][2]);
+                }
+                latitude = Double.parseDouble(userArray.getJSONObject(0).getString("latitude").toString());
+                longitude = Double.parseDouble(userArray.getJSONObject(0).getString("longitude").toString());
+                Log.d("Infos","Login : "+login+", latitude : "+latitude+", longitude : "+longitude);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
