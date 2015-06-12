61da2131f2c58a5a4e07938536ce4ea727583a32
Pierre Davy <pierredavy92@gmail.com>
2015-06-12 12:49:06 +0200

    retrait des ID

--- PHP/connexion.php
+++ PHP/connexion.php
@@ -1,9 +1,9 @@
-<?php
-	$host_name = "db554207254.db.1and1.com";
-    $database = "db554207254";
-    $dsn = 'mysql:dbname='.$database.';host='.$host_name;
-    $user_name = "dbo554207254";
-    $pwd = "if26ccool";
+﻿<?php
+	$host_name = "";
+    $database = "";
+    $dsn = ;
+    $user_name = "";
+    $pwd = "";
 	try {
 		$bdd = new PDO($dsn, $user_name, $pwd);
 	} catch (PDOException $e) {
